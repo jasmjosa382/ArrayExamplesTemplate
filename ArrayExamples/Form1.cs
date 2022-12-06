c3fe177363a10eb31fa70699faffdadcc64f87fa
jasmjosa382 <jasmjosa382@ed.amdsb.ca>
2022-12-06 10:56:16 -0500

    Finished

--- ArrayExamples/Form1.cs
+++ ArrayExamples/Form1.cs
@@ -29,7 +29,8 @@ namespace ArrayExamples
 
             for (int i = 0; i < numbers.Length; i++)
             {
-
+                numbers[i]++;
+                q1Output.Text += $"{numbers[i]} ";
             }
 
             /// 2. Sum all the values in the given array and 
@@ -40,7 +41,12 @@ namespace ArrayExamples
 
             int sum = 0;
 
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                sum = sum + numbers[i];
+            }
 
+            q2Output.Text += $"{sum} ";
 
             /// 3. Perform the following on the given array, then 
             /// display the updated array values.
@@ -54,7 +60,18 @@ namespace ArrayExamples
             /// 
             /// Sample Output
             /// -6 8 24 6 5
+            numbers[1] = numbers[1] + 5;          //numbers[1] += 5;
+            numbers[2] = numbers[2] * numbers[4]; // numbers[2] *= numbers[4];
+            numbers[0] = numbers[0] - numbers[1]; // numbers[0] -= numbers[1];
 
+            int temp = numbers[3];
+            numbers[3] = numbers[4];
+            numbers[4] = temp;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                q3Output.Text += $"{numbers[i]} ";
+            }
 
 
         }
